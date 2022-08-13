f2b5c1c3910eb83dfd532e368dc6175115f40aa6
Henok Zelalem <z.w.henok@gmail.com>
2022-08-13 08:07:07 +0300

    update

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,62 +3,62 @@ const ApiError = require("../utils/ApiError");
 const catchAsync = require("../utils/catchAsync");
 const { productService } = require("../services");
 const fileStructure = catchAsync(async (req, res, next) => {
-	console.log(req.body);
+  console.log(req.body);
 
-	if (req.files) {
-		req.files.map((file) => {
-			if (file["fieldname"].split("_").length > 1) {
-				const splt = file["fieldname"].split("_");
-				req.body["images"][splt[1]]["url"] = file["filename"];
-			} else {
-				req.body[file["fieldname"]] = file["filename"];
-			}
-			return "";
-		});
-	}
-	// console.log('inside file structure',req.body['types'][0]);
-	// console.log(req.body['types'][0]);
-	next();
+  if (req.files) {
+    req.files.map((file) => {
+      if (file["fieldname"].split("_").length > 1) {
+        const splt = file["fieldname"].split("_");
+        req.body["images"][splt[1]]["url"] = file["filename"];
+      } else {
+        req.body[file["fieldname"]] = file["filename"];
+      }
+      return "";
+    });
+  }
+  // console.log('inside file structure',req.body['types'][0]);
+  // console.log(req.body['types'][0]);
+  next();
 });
 const createProduct = catchAsync(async (req, res) => {
-	const product = await productService.createProduct(req.body);
-	res.send(product.toJSON());
+  const product = await productService.createProduct(req.body);
+  res.send(product.toJSON());
 });
 
 const getNestedProduct = catchAsync(async (req, res) => {
-	const product = await productService.getNestedProduct();
-	res.send(product);
+  const product = await productService.getNestedProduct();
+  res.send(product);
 });
 const getMyproducts = catchAsync(async (req, res) => {
-	const products = await productService.getMyproducts(req.user["_id"]);
-	res.send(products);
+  const products = await productService.getMyproducts(req.user["_id"]);
+  res.send(products);
 });
 const editProduct = catchAsync(async (req, res) => {
-	const product = await productService.editProduct(req.body);
-	res.send(product);
+  const product = await productService.editProduct(req.body);
+  res.send(product);
 });
 const deleteMyProduct = catchAsync(async (req, res) => {
-	const deletetd = await productService.deleteMyProduct(
-		req.query["pid"],
-		req.user["_id"]
-	);
-	console.log();
+  const deletetd = await productService.deleteMyProduct(
+    req.query["pid"],
+    req.user["_id"]
+  );
+  console.log();
 
-	if (deletetd.deletedCount != 0) return res.send(deletetd);
-	res.status(403).send("failed to delete");
+  if (deletetd.deletedCount != 0) return res.send(deletetd);
+  res.status(403).send("failed to delete");
 });
 
 const getHomeProducts = catchAsync(async (req, res) => {
-	const product = await productService.getHomeProducts();
-	res.send(product);
+  const product = await productService.getHomeProducts();
+  res.send(product);
 });
 
 module.exports = {
-	createProduct,
-	fileStructure,
-	getNestedProduct,
-	getMyproducts,
-	editProduct,
-	deleteMyProduct,
-	getHomeProducts,
+  createProduct,
+  fileStructure,
+  getNestedProduct,
+  getMyproducts,
+  editProduct,
+  deleteMyProduct,
+  getHomeProducts,
 };
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -8,32 +8,33 @@ const upload = require("../middlewares/imageupload");
 const router = express.Router();
 
 router
-	.route("/")
-	.post(
-		upload("images", "image/").any(),
-		productController.fileStructure,
-		auth(["Admin"], ["body", "SID"]),
-		validate(productValidation.createProduct),
-		productController.createProduct
-	)
-	.get(productController.getHomeProducts);
+  .route("/")
+  .post(
+    upload("images", "image/").any(),
+    productController.fileStructure,
+    auth(["Admin"], ["body", "SID"]),
+    validate(productValidation.createProduct),
+    productController.createProduct
+  )
+  .get(productController.getHomeProducts);
 
 router.route("/my-product").get(auth([], []), productController.getMyproducts);
 router.route("");
 router
-	.route("update")
-	.post(
-		upload("images", "image/").any(),
-		productController.fileStructure,
-		validate(productValidation.productUpdate),
-		productController.editProduct
-	);
+  .route("/update")
+  .post(
+    upload("images", "image/").any(),
+    productController.fileStructure,
+    auth([], ["body", "SID"]),
+    validate(productValidation.productUpdate),
+    productController.editProduct
+  );
 router
-	.route("/delete-my-product")
-	.get(
-		validate(productValidation.ProductID),
-		auth(["Admin"], []),
-		productController.deleteMyProduct
-	);
+  .route("/delete-my-product")
+  .get(
+    validate(productValidation.ProductID),
+    auth(["Admin"], []),
+    productController.deleteMyProduct
+  );
 
 module.exports = router; //
